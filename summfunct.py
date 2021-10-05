b3d9112664d4fcd070f9682635c592a0eed1a974
stevendreed <91928433+stevendreed@users.noreply.github.com>
2021-10-05 11:23:34 -0700

    Create summfunct.py

---
+++ summfunct.py
@@ -0,0 +1,24 @@
+# function defs
+
+def summ_calc():
+"""Program adds N-j quantities of f(x) together, incrementing f(x) by 1 each loop"""
+	if N >= j and N > 0:
+		summ_iter_var = N - j
+		while summ_iter_var > 0:
+			
+def summ_x_to_first():
+"""Program optimization which uses (N(N+1))/2 rule only when f(x)= Kx + C"""
+	print("summ_x_to_first test")
+
+def summ_x_to_second():
+"""Program optimization which uses (N(N+1)(2N+1))/6 rule only when f(x) = Kx^{2} + C"""
+	print("summ_x_to_second test")
+
+def summ_x_to_third():
+"""Program optimization which uses [forgot this rule lol] only when f(x) = Kx^{3} + C"""
+	print("summ_x_to_third test")
+
+def funct_in_con():
+"""Function translates user inputs into arithmatic meaning"""
+	
+	# print("funct_in_con() test")
