983dbf158f435e4ea5443c98bc75cf1b51d2e0f1
stevendreed <91928433+stevendreed@users.noreply.github.com>
2021-10-05 11:22:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,13 @@
 # secondary goal: program summation rules
 # I think I need to program a simple additive program first?
 
-# v 1.1 code below
-
 # variable seeds
 N = 5;
 j = 1;
 funct_x = "x";
+funct_ele = [];
 summ = 0.0;
 summ_iter_var = 0;
-
-# function defs
-
-def summ_calc():
-"""Program adds N-j quantities of f(x) together, incrementing f(x) by 1 each loop"""
-	if N >= j and N > 0:
-		summ_iter_var = N - j
-		while summ_iter_var > 0:
-			
-
-def summ_x_to_first():
-"""Program optimization which uses (N(N+1))/2 rule only when f(x)= Kx + C"""
-	print("summ_x_to_first test")
-
-def summ_x_to_second():
-"""Program optimization which uses (N(N+1)(2N+1))/6 rule only when f(x) = Kx^{2} + C"""
-	print("summ_x_to_second test")
-
-def summ_x_to_third():
-"""Program optimization which uses [forgot this rule lol] only when f(x) = Kx^{3} + C"""
-	print("summ_x_to_third test")
-
-def funct_in_con():
-"""Function translates user inputs into arithmatic meaning"""
-	
-	# print("funct_in_con() test")
 
 # user instructions
 """Hello, welcome to the Summation Calculator v. 1.1
